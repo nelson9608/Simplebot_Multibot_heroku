9d48d7512777438ebabcda46b5d1e565c0ad7c9d
Nelson <104731311+nelson9608@users.noreply.github.com>
2022-12-29 05:36:02 -0500

    Add files via upload

---
+++ heroku.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# configure the bot
+python3 -m simplebot init "$ADDR" "$PASSWORD"
+python3 -m simplebot -a "$ADDR" set_name "$BOT_NAME"
+python3 ./restore_keys.py
+
+# add admin plugin
+if [ -n "$ADMIN" ]; then
+    python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/admin.py'); open('admin.py', 'wb').write(r.content)"
+    python3 -m simplebot -a "$ADDR" plugin --add ./admin.py
+    python3 -m simplebot -a "$ADDR" admin --add "$ADMIN"
+fi
+
+# start the bot
+python3 -m simplebot -a "$ADDR" serve
